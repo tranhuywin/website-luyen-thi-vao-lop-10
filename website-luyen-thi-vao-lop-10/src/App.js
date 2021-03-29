cd456171fed274ea75c966920f65323fd509a6ff
Win Trần Huy <18521686@ms.uit.edu.vn>
2021-03-29 16:05:57 +0700

    first commit

---
+++ website-luyen-thi-vao-lop-10/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import VerticalTabs from "./Components/Homepage/VerticalTabs";
+import HomeTab from "./Components/Homepage/HomeTab";
+
+function App() {
+
+  return (
+    <div className="App">
+    	<Grid container spacing={1}>
+        
+    		<Grid item xs={12}>
+                <HomeTab/>
+        	</Grid>
+        	<Grid item xs={3}>
+				<VerticalTabs/>
+        	</Grid>
+        	<Grid item xs={9}>
+            	right
+        	</Grid>
+      	</Grid>
+		  
+    </div>
+  );
+}
+
+export default App;
